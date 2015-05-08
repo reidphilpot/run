9c406618a1923c1fc54e089772e6b13887b9e7db
Reid Philpot <reidphilpot@gmail.com>
2015-05-08 17:24:19 +0100

    tabs to spaces

--- run.js
+++ run.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
 var path = require('path')
-	, express = require('express')
-	, keypress = require('keypress')
-	, open = require('open')
-	, portfinder = require('portfinder')
-	, app = express()
-	, target
+  , express = require('express')
+  , keypress = require('keypress')
+  , open = require('open')
+  , portfinder = require('portfinder')
+  , app = express()
+  , target
 
 keypress(process.stdin)
 
 process.stdin.on('keypress', function (ch, key) {
   if (key && key.ctrl) {
-  	switch(key.name) {
-  		case 'o':
-  			open(target)
-  			break
-			case 'c':
-				process.exit(0)
-				break
-  	}
+    switch(key.name) {
+      case 'o':
+        open(target)
+        break
+      case 'c':
+        process.exit(0)
+        break
+    }
   }
 })
 
@@ -31,14 +31,14 @@ app.use('/', express.static(process.cwd()))
 
 portfinder.getPort(function(err, port) {
 
-	target = 'http://localhost:' + port
+  target = 'http://localhost:' + port
 
-	app.listen(port)
+  app.listen(port)
 
-	console.log('server started: ' + target)
-	console.log('-----')
-	console.log('ctrl + o to open browser')
-	console.log('ctrl + c to exit')
-	console.log('-----')
+  console.log('server started: ' + target)
+  console.log('-----')
+  console.log('ctrl + o to open browser')
+  console.log('ctrl + c to exit')
+  console.log('-----')
 
 })
